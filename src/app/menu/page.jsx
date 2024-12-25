e45d422df38a2ec81e3edd48ac3d1266bb8df781
Pei Shang <stean8861@gmail.com>
2024-12-26 00:45:50 +0800

    更新懶加載

--- src/app/menu/page.jsx
+++ src/app/menu/page.jsx
@@ -1,10 +1,11 @@
 import { getFood } from "@/action/menu";
-import Filter from "@/components/Filter";
-import MenuList from "@/components/menu/MenuList";
+import { lazy, Suspense } from "react";
 import Slider from "@/components/Slider";
 import LoadingSpinner from "@/components/LoadingSpinner";
-import { Suspense } from "react";
 import { redirect } from "next/navigation";
+
+const Filter = lazy(() => import("@/components/Filter"));
+const MenuList = lazy(() => import("@/components/menu/MenuList")); //懶加載
 
 export const metadata = {
   title: "商品一覽",
@@ -23,8 +24,8 @@ async function page({ searchParams }) {
   const sortBy = searchParams?.sort ?? "default";
   const query = searchParams?.query ?? "";
 
-   // 如果未提供查詢參數，重定向到預設網址
-   if (!searchParams?.category || !searchParams?.sort) {
+  // 如果未提供查詢參數，重定向到預設網址
+  if (!searchParams?.category || !searchParams?.sort) {
     redirect(`/menu?category=全部&sort=default`);
   }
 
@@ -40,7 +41,8 @@ async function page({ searchParams }) {
     if (sortBy === "desc") return b.basePrice - a.basePrice; // 價格降序
     return 0; // 預設排序
   });
-  //過濾搜尋
+
+  // 過濾搜尋
   const searchedFood = sortedFood.filter((item) =>
     item.name.toLowerCase().includes(query.toLowerCase())
   );
@@ -54,21 +56,22 @@ async function page({ searchParams }) {
   ];
 
   return (
-    <section className=" min-h-screen mb-16">
+    <section className="min-h-screen mb-16">
       <div className="space-y-6">
         <Slider />
         {/* 過濾 */}
-        <Filter
-          query={query}
-          categories={categories}
-          currentFilter={filter}
-          currentSort={sortBy}
-        />
+        <Suspense fallback={<LoadingSpinner />}>
+          <Filter
+            query={query}
+            categories={categories}
+            currentFilter={filter}
+            currentSort={sortBy}
+          />
+        </Suspense>
 
         {/* 食物清單 */}
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 ">
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
           {searchedFood.length === 0 ? (
-            // 顯示無結果提示
             <p className="text-center text-brown-50 col-span-full pt-8">
               查無符合的食物。
             </p>
